5266772537947d062c2013176fd86ba28e621940
Feisal Fahmi <feisalfahmi@gmail.com>
2019-12-29 15:48:43 +0700

    xx

--- crud_firebase_flutter/lib/main.dart
+++ crud_firebase_flutter/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'myProject.dart';
+import 'login.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,64 +18,76 @@ class MyApp extends StatelessWidget {
     );
   }
 }
- class MyHomePage extends StatefulWidget {
-   @override
-   _MyHomePageState createState() => _MyHomePageState();
- }
- 
- class _MyHomePageState extends State<MyHomePage> {
-    final FirebaseAuth _auth = FirebaseAuth.instance;
-    final GoogleSignIn googleSignIn = GoogleSignIn();
 
-  
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
   Future<String> signInWithGoogle() async {
-      final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-      final GoogleSignInAuthentication googleSignInAuthentication =
-          await googleSignInAccount.authentication;
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
-   final FirebaseUser user = await _auth.signInWithCredential(credential);
-     Navigator.of(context).push(
-     new MaterialPageRoute(
-       builder:(BuildContext context)=> new MyProject(
-         user:user,
-         googleSignIn:googleSignIn)
-     )
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
     );
-   }
- 
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
+    final FirebaseUser user = await _auth.signInWithCredential(credential);
+    Navigator.of(context).push(new MaterialPageRoute(
+        builder: (BuildContext context) =>
+            new MyProject(user: user, googleSignIn: googleSignIn)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/BackgroundLogin.jpg"), 
-            fit: BoxFit.cover 
-          )  
-        ),
+            image: DecorationImage(
+                image: AssetImage("assets/images/BackgroundLogin.jpg"),
+                fit: BoxFit.cover)),
         child: Padding(
           padding: const EdgeInsets.only(top: 50),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-            new Image.asset("assets/images/logo.png",height: 150.0,width : 150.0,),
-            new Padding(padding: EdgeInsets.only(top: 10),),
-            new InkWell(
-              onTap: (){
-                signInWithGoogle();
-              },
-              child: new Image.asset(
-                "assets/images/signin.png",
-                height: 60.0,
-                width : 150.0,)),
-          ],),
+              new Image.asset(
+                "assets/images/logo.png",
+                height: 170.0,
+                width: 170.0,
+              ),
+              new Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              new InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) =>
+                      new Login()));
+                  },
+                  child: new Image.asset(
+                    "assets/images/login1.png",
+                    height: 60.0,
+                    width: 150.0,
+                  )),
+              new InkWell(
+                  onTap: () {
+                    signInWithGoogle();
+                  },
+                  child: new Image.asset(
+                    "assets/images/signin.png",
+                    height: 60.0,
+                    width: 150.0,
+                  )),
+            ],
+          ),
         ),
-      ), 
+      ),
     );
   }
 }
